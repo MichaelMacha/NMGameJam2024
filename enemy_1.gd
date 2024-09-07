d080b0ff170bef61f4b14af686af3808a2003ca0
Michael Macha <vikinghelmet99@gmail.com>
2024-09-07 06:33:19 -0600

    Added an enemy spawner
    The spawner assumes we have a play field of 640 × 360. It spawns a
    slight offset (currently a hard 20 pixels) outside of this.

--- enemy_1.gd
+++ enemy_1.gd
@@ -1,7 +1,7 @@
 class_name Enemy extends CharacterBody2D
 
 @export var hearts := 3
-@export var movement_speed := 20.0
+@export var movement_speed := 40.0
 
 @onready var hero = $/root/World/Hero
 
