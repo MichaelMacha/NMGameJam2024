52171ae1c24b9205bd914c615949a930c7dbc19d
Classified3939 <102702965+Classified3939@users.noreply.github.com>
2024-09-07 08:37:35 -0600

    add menu to test branch

---
+++ main_menu.gd
@@ -0,0 +1,25 @@
+extends MarginContainer
+@onready var animPlayer = $AnimationPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	animPlayer.play("main-pulse");
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+
+func _on_play_button_pressed() -> void:
+	#get_tree().change_scene_to_file("res://CHANGE-ME-TO-GAME-SCENE");
+	pass;
+
+
+func _on_options_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://options_menu.tscn");
+	pass;
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit();
---
+++ options_menu.gd
@@ -0,0 +1,25 @@
+extends MarginContainer
+@onready var animPlayer = $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	animPlayer.play("options-pulse")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+
+func _on_volume_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(0,linear_to_db(value));
+	
+	
+
+
+func _on_mute_toggled(toggled_on: bool) -> void:
+	AudioServer.set_bus_mute(0,toggled_on);
+
+
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://main_menu.tscn");
