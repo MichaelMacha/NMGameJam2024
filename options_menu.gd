6ba41d2d0bdea38b4fafd601d5c47bbe81ff9782
Classified3939 <102702965+Classified3939@users.noreply.github.com>
2024-09-07 19:02:16 -0600

    fullscreen option

--- options_menu.gd
+++ options_menu.gd
@@ -3,7 +3,7 @@ extends MarginContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	animPlayer.play("options-pulse")
+	animPlayer.play("options-pulse");
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,9 +13,6 @@ func _process(_delta: float) -> void:
 
 func _on_volume_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(0,linear_to_db(value/100));
-	
-	
-
 
 func _on_mute_toggled(toggled_on: bool) -> void:
 	AudioServer.set_bus_mute(0,toggled_on);
@@ -23,3 +20,7 @@ func _on_mute_toggled(toggled_on: bool) -> void:
 
 func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://main_menu.tscn");
+
+
+func _on_fullscreen_toggled(toggled_on: bool) -> void:
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN) if toggled_on else DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED);
